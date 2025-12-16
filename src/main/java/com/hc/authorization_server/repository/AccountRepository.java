1ac25c89d2712b20d422a0db86663bed59ad7281
user <email>
2025-12-16 17:54:29 -0300

    db-user-details-service

---
+++ src/main/java/com/hc/authorization_server/repository/AccountRepository.java
@@ -0,0 +1,23 @@
+package com.hc.authorization_server.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.hc.authorization_server.entity.Account;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+	//@Query("SELECT a FROM Account a WHERE a.email = :email")
+	
+	@Query(value = "select * from get_account_authorities(:email)", nativeQuery = true)
+	Optional<Account> findByEmail(@Param("email") String email);
+	
+	//"SELECT a FROM Account a WHERE a.identifier = :identifier"
+	
+	@Query(value = "select * from get_account_byid_authorities(:identifier)", nativeQuery = true)
+	Optional<Account> findByIdentifier(@Param("identifier") UUID identifier);
+}
+
